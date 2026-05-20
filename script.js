4ee3585f910d522cbf80a4e4e7babc9be5b3c116
rafaelfreitasdeassis <rafaeldoze@hotmail.com>
2026-05-20 20:28:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const API_URL = 'html://localhost:3000/usuarios';       // Configuração da URL base da API
+
+const formUsuario = document.getElementById('form-usuario');   // Seleção de elementos do DOM
+
+const payload = {
+    nome: document
+}
